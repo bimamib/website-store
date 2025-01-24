520764d7f8df3fe48369174b91a957453b7b04f1
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-01-24 23:36:02 +0700

    Edit Home Page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-  return <div>Home Page</div>;
+import getBanner from "@/actions/get-banner";
+import Banner from "@/components/banner";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const banner = await getBanner("bf9aea0d-f6b6-49ba-b272-b30987a68019");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10 ">
+        <Banner data={banner} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
