3638c991576a3a798c66e25ab0bec31fb240b430
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-01-27 23:36:40 +0700

    Add Product List & No Product

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types";
+import NoResults from "./ui/no-results";
 
 interface ProductListProps {
   title: string;
@@ -9,6 +10,7 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
   return (
     <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
     </div>
   );
 };
